672335cac3b401b78611038dc4946231538ececa
scastlara <s.cast.lara@gmail.com>
2018-09-04 10:17:39 +0200

    settings

--- netcutter-web/settings.py
+++ netcutter-web/settings.py
@@ -62,11 +62,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR + '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
