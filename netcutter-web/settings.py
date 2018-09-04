673289ececaf3f083b86818d7c9cc5de5e96c623
scastlara <s.cast.lara@gmail.com>
2018-09-04 10:26:32 +0200

    static url?

--- netcutter-web/settings.py
+++ netcutter-web/settings.py
@@ -66,10 +66,11 @@ STATIC_ROOT = BASE_DIR + '/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = BASE_DIR + '/static/'
+STATIC_URL = 'http://localhost:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    BASE_DIR + '/static/'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
